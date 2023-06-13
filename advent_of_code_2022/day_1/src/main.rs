b4d2eac4c4aeac2954abbb4aa281907dd30df0b7
Ayush789 <ayush999g@gmail.com>
2023-06-14 03:03:28 +0530

    feat: D1.1

--- advent_of_code_2022/day_1/src/main.rs
+++ advent_of_code_2022/day_1/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn main(){
+    part1();
+}
+
+fn part1() {
+    let filename = "input1.txt".to_string();
+    let data = fs::read_to_string(filename).unwrap();
+
+    let ans: i32 = data
+        .split("\r\n\r\n")
+        .map(|x| x.split("\r\n").map(|v| v.parse::<i32>().unwrap()).sum())
+        .max()
+        .unwrap();
+    dbg!(ans);
 }
